486c5941defdcc8f2e97cea5fe945eac92cba45a
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-06-04 00:44:34 +0700

    Success tampilkan google maps

---
+++ lib/ui/google_maps_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapsScreen extends StatefulWidget {
+  const GoogleMapsScreen({Key? key}) : super(key: key);
+  @override
+  _GoogleMapsScreenKaryawanState createState() =>
+      _GoogleMapsScreenKaryawanState();
+}
+
+class _GoogleMapsScreenKaryawanState extends State<GoogleMapsScreen> {
+  final Set<Marker> _markers = {};
+  final LatLng _currentPosition =
+      const LatLng(0.9211113485406496, 104.45414034206269);
+
+  @override
+  void initState() {
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("0.9211113485406496, 104.45414034206269"),
+        position: _currentPosition,
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Google Maps View",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: _currentPosition,
+          zoom: 17.0,
+        ),
+        markers: _markers,
+      ),
+    );
+  }
+}
