0dc355931b209882f4ad219d78b89b8f9b501f2b
falfikri <fazliialfikri@gmail.com>
2022-06-10 19:39:37 +0700

    siuuuuuuuu

--- lib/ui/google_maps_screen.dart
+++ lib/ui/google_maps_screen.dart
@@ -13,6 +13,9 @@ class _GoogleMapsScreenKaryawanState extends State<GoogleMapsScreen> {
 
   final LatLng _currentPosition = const LatLng(0.9211113485406496, 104.45414034206269);
   final LatLng _currentPositionHome = const LatLng(0.930880, 104.522857);
+  final LatLng mall_tcc = const LatLng(0.8942866230087267, 104.47956706874625);
+  final LatLng kb_dompak = const LatLng(0.8763923993562516, 104.44514194616016);
+  final LatLng k_walkottpi = const LatLng(0.9653413671650932, 104.44089448494357);
 
   @override
   void initState() {
@@ -28,6 +31,29 @@ class _GoogleMapsScreenKaryawanState extends State<GoogleMapsScreen> {
       Marker(
         markerId: const MarkerId("0.930880, 104.522857"),
         position: _currentPositionHome,
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("0.8942866230087267, 104.47956706874625"),
+        position: mall_tcc,
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("0.8763923993562516, 104.44514194616016"),
+        position: kb_dompak,
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("0.9653413671650932, 104.44089448494357"),
+        position: k_walkottpi,
         icon: BitmapDescriptor.defaultMarker,
       ),
     );
